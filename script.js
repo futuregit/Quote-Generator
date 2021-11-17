edb5624fe3b5dec3a5dd5d2c17d42d2b2c729043
futuregit <gmail.com>
2021-11-17 05:36:38 -0500

    Added loader

--- script.js
+++ script.js
@@ -1,34 +1,71 @@
+let quoteContainer = document.getElementById('quote-container');
 let quote = document.getElementById('quote');
 let writer = document.getElementById('author');
 let newQuotes = document.getElementById('new-quote');
+let twitterBtn = document.getElementById('twitter');
+let loader = document.getElementById('loader');
 
 let apiQuotes = [];
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
 
 // Show New Quote
 function newQuote() {
     // Pick a random quote from apiQuotes array
-    const ranQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    return ranQuote;
+    const fetchQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // Check to see if author is null
+    if(!fetchQuote.author){
+        writer.textContent = 'Unknown'
+    } else {
+        writer.textContent = fetchQuote.author
+    }
+    // Check quote text Length to determine styling
+    quote.classList.toggle('long-quote', fetchQuote.text.length > 50);
+
+    // Set quote, hide loader
+    quote.textContent = fetchQuote.text;    
+    complete();
+    
 }
 
-newQuotes.addEventListener('click', getQuotes);
+
 
 // Get Quotes FROM API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        let { text, author } = newQuote();
-        quote.innerHTML = text
-        if(author === null || undefined){
-            author = 'Unknown'
-        }
-        writer.innerHTML = author
+        newQuote();
     } catch (err) {
         // Catch Error Here
     }
 }
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote.textContent} - ${writer.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event listeners
+
+// Add button event listener for quote
+newQuotes.addEventListener('click', getQuotes);
+
+// Add twitter button event listener
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
